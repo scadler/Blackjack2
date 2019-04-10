f511ffeb35f099062e28b52439675ffa75d03f26
Scott Adler <anorangepi@gmail.com>
2019-04-10 00:24:36 +0000

    api finally works

---
+++ js/script.js
@@ -0,0 +1,28 @@
+/* global $ */
+
+$("#start").click(function(){
+    $.ajax({
+    url: "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=8",
+    method: "GET",
+     success: function(response) {
+         var deckID = `${response.deck_id}`;
+         console.log(deckID);
+         getHand(deckID);
+     } 
+ });
+ $("#playerHand").append("test");
+});
+function getHand(deckID){
+ console.log(deckID);
+ $.ajax({
+    url: "https://deckofcardsapi.com/api/deck/"+ deckID +"/draw/?count=2",
+    method: "GET",
+    success: function(response){
+         $("#playerHand").append(`<img src=${response.cards[0].image}\>`)
+         $("#playerHand").append(`<img src=${response.cards[1].image}\>`);
+         }, 
+},
+)}
+
+
+//   link to api site ---> https://deckofcardsapi.com/
